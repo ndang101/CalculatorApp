912dc6cc9ea4327d9cb8664e8571d7f5c120fc5a
Nathan Dang <ndang5@huskers.unl.edu>
2025-11-24 13:55:12 -0600

    Implement basic calculator UI with buttons and display

---
+++ app.py
@@ -0,0 +1,94 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout)
+
+from PyQt5.QtCore import Qt
+
+class CalculatorApp(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle("Calculator App")
+
+    #initialize variables
+    self.button_0 = QPushButton("0", self)
+    self.button_1 = QPushButton("1", self)
+    self.button_2 = QPushButton("2", self)
+    self.button_3 = QPushButton("3", self)
+    self.button_4 = QPushButton("4", self)
+    self.button_5 = QPushButton("5", self)
+    self.button_6 = QPushButton("6", self)
+    self.button_7 = QPushButton("7", self)
+    self.button_8 = QPushButton("8", self)
+    self.button_9 = QPushButton("9", self)
+
+
+    self.display = QLineEdit()
+
+    # operators
+    self.add_button = QPushButton("+", self)
+    self.subtract_button = QPushButton("-", self)
+    self.multiply_button = QPushButton("x", self)
+    self.divide_button = QPushButton("÷", self)
+    self.calculate_button = QPushButton("=", self)
+    self.clear_button = QPushButton("C", self)
+    # self.delete_button = QPushButton("⌫", self)
+
+    self.initUI()
+    
+  def initUI(self):
+    # Sets window title
+    self.setWindowTitle("Calculator App")
+
+    # Changes display properties
+    self.display.setReadOnly(True)
+    self.display.setAlignment(Qt.AlignRight)
+
+    # initialize 4 columns for each button
+    vbox1 = QVBoxLayout()
+    vbox2 = QVBoxLayout()
+    vbox3 = QVBoxLayout()
+    vbox4 = QVBoxLayout()
+
+    vbox1.addWidget(self.button_7)
+    vbox1.addWidget(self.button_4)
+    vbox1.addWidget(self.button_1)
+    vbox1.addWidget(self.clear_button)
+
+    vbox2.addWidget(self.button_8)
+    vbox2.addWidget(self.button_5)
+    vbox2.addWidget(self.button_2)
+    vbox2.addWidget(self.button_0)
+
+    vbox3.addWidget(self.button_9)
+    vbox3.addWidget(self.button_6)
+    vbox3.addWidget(self.button_3)
+    vbox3.addWidget(self.calculate_button)
+
+    vbox4.addWidget(self.add_button)
+    vbox4.addWidget(self.subtract_button)
+    vbox4.addWidget(self.multiply_button)
+    vbox4.addWidget(self.divide_button)
+
+    # Puts the 4 columns into 4 rows
+    hbox = QHBoxLayout()
+
+    hbox.addLayout(vbox1)
+    hbox.addLayout(vbox2)
+    hbox.addLayout(vbox3)
+    hbox.addLayout(vbox4)
+
+    # Adds a VBox to allow for display to go on top
+    main_layout = QVBoxLayout()
+    main_layout.addWidget(self.display) # Display goes first
+    main_layout.addLayout(hbox) # Button columns go under it
+
+    self.setLayout(main_layout)
+  
+  def operation():
+     pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    CalculatorApp = CalculatorApp()
+    CalculatorApp.show()
+    sys.exit(app.exec_())
